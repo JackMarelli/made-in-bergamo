2de49e859f71ec474ec96666a2b165da9d930203
JackMarelli <marelligiacomo@gmail.com>
2024-04-03 19:59:28 +0200

    image refs

--- src/components/ImageRect/ImageRect.jsx
+++ src/components/ImageRect/ImageRect.jsx
@@ -1,39 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
-export default function ImageRect({ imageUrl, altText = "", dida = "", className }) {
-    const [isHovering, setIsHovering] = useState(false);
-    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+export default function ImageRect({
+  imageUrl,
+  altText = "",
+  dida = "",
+  className,
+}) {
+  const [isHovering, setIsHovering] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const containerRef = useRef(null);
+  const imageRef = useRef(null);
 
-    const handleMouseEnter = () => setIsHovering(true);
-    const handleMouseMove = (event) => {
-        if (isHovering) {
-            setMousePosition({
-                x: event.clientX,
-                y: event.clientY,
-            });
-        }
-    };
-    const handleMouseLeave = () => setIsHovering(false);
+  const handleMouseEnter = () => setIsHovering(true);
+  const handleMouseMove = (event) => {
+    if (isHovering) {
+      setMousePosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    }
+  };
+  const handleMouseLeave = () => setIsHovering(false);
 
-    // Prepare the styles for the div that should appear when hovering.
-    const followerStyle = {
-        position: 'fixed',
-        top: mousePosition.y,
-        left: mousePosition.x,
-        transform: 'translate(-50%, -50%)',
-        display: isHovering ? 'block' : 'none',
-        pointerEvents: 'none', // Make sure the div doesn't interfere with mouse events.
-    };
+  // Prepare the styles for the div that should appear when hovering.
+  const followerStyle = {
+    position: "fixed",
+    top: mousePosition.y,
+    left: mousePosition.x,
+    transform: "translate(-50%, -50%)",
+    display: isHovering ? "block" : "none",
+    pointerEvents: "none", // Make sure the div doesn't interfere with mouse events.
+  };
 
-    return (
-        <div
-            className={`${className} h-fit relative`} // Ensuring the container has a relative positioning.
-            onMouseEnter={handleMouseEnter}
-            onMouseMove={handleMouseMove}
-            onMouseLeave={handleMouseLeave}
-        >
-            <img src={imageUrl} className="w-full h-auto" alt={altText} />
-            <div style={followerStyle}>{dida}</div>
-        </div>
-    );
+  return (
+    <div
+      ref={containerRef}
+      className={`${className} h-fit relative`} // Ensuring the container has a relative positioning.
+      onMouseEnter={handleMouseEnter}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+    >
+      <img
+        ref={imageRef}
+        src={imageUrl}
+        className="w-full h-auto"
+        alt={altText}
+      />
+      <div style={followerStyle}>{dida}</div>
+    </div>
+  );
 }
