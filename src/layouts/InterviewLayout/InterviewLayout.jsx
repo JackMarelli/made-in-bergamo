82b821f11d6b1a70b8238e050015902ce52261ed
JackMarelli <marelligiacomo@gmail.com>
2024-04-23 15:26:26 +0200

    Update InterviewLayout.jsx

--- src/layouts/InterviewLayout/InterviewLayout.jsx
+++ src/layouts/InterviewLayout/InterviewLayout.jsx
@@ -9,34 +9,34 @@ export default function InterviewLayout({ props, children }) {
     <>
       <BaseLayout>
         <div className="w-full h-fit py-16 md:py-24 px-4 md:py-32 md:px-8 xl:px-12 grid grid-cols-4 md:grid-cols-12 gap-4 md:gap-y-0 bg-mib-beige-light min-h-screen">
-          <div className="hidden md:block h-fit md:col-start-2 md:col-span-3 pe-4 sticky top-32">
+          <div className="hidden md:block h-fit md:col-span-3 fixed left-0 top-1/2 -translate-y-1/2">
             <Link
               to="/interview/neimieipanni"
-              className="my-4 text-sm md:text-base lg:text-2xl font-semibold leading-[1.2em] cursor-pointer block"
+              className="px-0 py-2 text-sm md:text-xs lg:text-base font-semibold leading-[1.2em] cursor-pointer block"
             >
               Nei Miei Panni
             </Link>
             <Link
               to="/interview/sophiestique"
-              className="my-4 text-sm md:text-base lg:text-2xl font-semibold leading-[1.2em] cursor-pointer block"
+              className="px-0 py-2 text-sm md:text-xs lg:text-base font-semibold leading-[1.2em] cursor-pointer block"
             >
               Sophie Stique*
             </Link>
             <Link
               to="/interview/albinigroup"
-              className="my-4 text-sm md:text-base lg:text-2xl font-semibold leading-[1.2em] cursor-pointer block"
+              className="px-0 py-2 text-sm md:text-xs lg:text-base font-semibold leading-[1.2em] cursor-pointer block"
             >
               Albini Group
             </Link>
             <Link
               to="/interview/officinae"
-              className="my-4 text-sm md:text-base lg:text-2xl font-semibold leading-[1.2em] cursor-pointer block"
+              className="px-0 py-2 text-sm md:text-xs lg:text-base font-semibold leading-[1.2em] cursor-pointer block"
             >
               Officinae
             </Link>
             <Link
               to="/interview/ertkingdom"
-              className="my-4 text-sm md:text-base lg:text-2xl font-semibold leading-[1.2em] cursor-pointer block"
+              className="px-0 py-2 text-sm md:text-xs lg:text-base font-semibold leading-[1.2em] cursor-pointer block"
             >
               E RT Kingdom
             </Link>
