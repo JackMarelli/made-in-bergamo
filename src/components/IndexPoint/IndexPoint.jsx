c141c06c7e4844324d479be77083b1d9138583db
JackMarelli <marelligiacomo@gmail.com>
2024-06-04 14:45:39 +0200

    minor margin fixes

--- src/components/IndexPoint/IndexPoint.jsx
+++ src/components/IndexPoint/IndexPoint.jsx
@@ -1,10 +1,10 @@
 export default function IndexPoint({ children, className}) {
-    const containerClassName = `w-fit h-fit pt-1 mb-4 flex flex-row justify-start items-start gap-2 ${className} text-mib-brown-dark`;
+    const containerClassName = `w-fit h-fit pt-1 -mb-2 md:mb-4 flex flex-row justify-start items-start gap-2 ${className} text-mib-brown-dark`;
 
     return (
         <div className={containerClassName}>
             <div className={`hidden md:block w-[14px] h-[14px] rounded-full bg-mib-brown-dark mt-[2px]`}></div>
-            <div className="h-full w-fit uppercase text-[15px] md:text-[16px] lg:text-[18px] font-semibold leading-[1.2rem] tracking-[0px] select-none pointer-events-none">{children}</div>
+            <div className="h-full w-fit uppercase text-[16px] md:text-[16px] lg:text-[18px] font-semibold leading-[1.2rem] tracking-[0px] hyphens-auto select-none pointer-events-none" lang="it">{children}</div>
         </div>
     );
 }
--- src/components/InterviewQuestion/InterviewQuestion.jsx
+++ src/components/InterviewQuestion/InterviewQuestion.jsx
@@ -4,7 +4,7 @@ export default function InterviewQuestion({ question, children }) {
   return (
     <div className="w-full h-fit mb-12 md:mb-12">
       <h2 className="h-full w-fit font-semibold font-crimson-pro text-[1.4em] md:text-[1.7em] leading-6 md:leading-7 select-none mb-2">{question}</h2>
-      <div className="font-inter text-md md:text-lg leading-[1.4em] select-none">{children}</div>
+      <div className="font-inter text-md md:text-lg leading-[1.4em] select-none flex flex-col gap-4">{children}</div>
     </div>
   );
 }
