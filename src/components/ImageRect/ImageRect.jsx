6f954b721b7314afbafb07143677bc75e705db11
JackMarelli <marelligiacomo@gmail.com>
2024-06-06 10:37:58 +0200

    minor changes

--- src/components/ImageRect/ImageRect.jsx
+++ src/components/ImageRect/ImageRect.jsx
@@ -81,9 +81,10 @@ export default function ImageRect({
     transition: "opacity 300ms ease",
     zIndex: 9999,
     pointerEvents: "none",
-    whiteSpace: "nowrap",
     overflow: "hidden",
     userSelect: "none",
+    minWidth: "260px",
+    maxWidth: "100vw"
   };
 
   return (
@@ -102,7 +103,7 @@ export default function ImageRect({
       />
       <div
         style={followerStyle}
-        className="px-5 py-2 bg-mib-beige-light border-2 border-mib-brown-dark rounded-full font-semibold text-mib-brown-dark text-sm"
+        className="px-5 py-2 mx-2 bg-mib-beige-light border-2 border-mib-brown-dark rounded-full font-semibold text-mib-brown-dark text-sm text-center"
       >
         {dida}
       </div>
